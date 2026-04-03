086591277e4df79c8ac0a585cbfaec0110b63526
PhoenixJin23 <g3472863433@gmail.com>
2026-04-03 16:49:53 +0800

    对图书价格进行爬取

--- web_scraper/web_crawler_1.py
+++ web_scraper/web_crawler_1.py
@@ -14,9 +14,9 @@ def scrape_books():
     for book in name_list:
         name=book.h3.a["title"]
         price=soup.find("p",attrs={"class":"price_color"}).get_text()
-        print(f"书名：{name},价格：{price[1:]}")
+        star_classes=book.find("p",class_="star-rating")['class'] #标签['class']会返回列表[star-rating,three]
+        rating=star_classes[1] #取列表中的第2个元素
+        print(f"书名：{name},价格：{price[1:]},星级：{rating}")
 
 
 scrape_books()
-
-
