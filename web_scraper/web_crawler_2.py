785ca7f678cf3446e0720462fdfbed4fd65e0c1f
PhoenixJin23 <g3472863433@gmail.com>
2026-04-04 15:50:57 +0800

    新增翻页功能和程序试运行机制

---
+++ web_scraper/web_crawler_2.py
@@ -0,0 +1,28 @@
+import requests
+from bs4 import BeautifulSoup
+
+
+def scrape_books():
+    headers={"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/146.0.0.0 Safari/537.36 Edg/146.0.0.0"}
+    for start_num in range(1,51):
+        url=f"http://books.toscrape.com/catalogue/page-{start_num}.html"
+        print(f"正在爬取网站第{start_num}页")
+        try:
+            content=requests.get(url).text #提取文本
+            soup=BeautifulSoup(content,"html.parser")
+
+
+            name_list=soup.find_all("li",class_="col-xs-6 col-sm-4 col-md-3 col-lg-3")
+            print(f"成功发现{len(name_list)}本书！")
+            for book in name_list:
+                name=book.h3.a["title"]
+                price=soup.find("p",attrs={"class":"price_color"}).get_text()
+                star_classes=book.find("p",class_="star-rating")['class'] #标签['class']会返回列表[star-rating,three]
+                rating=star_classes[1] #取列表中的第2个元素
+                print(f"书名：{name},价格：{price[1:]},星级：{rating}")
+        except Exception as e: #如果出错了，抓所有类型的错误并存入e变量
+            print(f"爬取第{start_num}页时出错：{e}")
+            break
+
+
+scrape_books()
